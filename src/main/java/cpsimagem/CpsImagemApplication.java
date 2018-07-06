6bed79acbd405e9977a9ff82b7b5b466eeba34ad
NoeLevi <noelevytete@gmail.com>
2018-07-06 16:06:28 +0200

    fird

---
+++ src/main/java/cpsimagem/CpsImagemApplication.java
@@ -0,0 +1,14 @@
+package cpsimagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpsImagemApplication {
+public static void main (String [] args){
+		
+        SpringApplication.run(CpsImagemApplication.class, args);
+		
+	}
+}
